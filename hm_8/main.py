02a4c1d5a20a1235efcdd00df23e15c663c417ef
Diana Volynets <d.volynets999@gmail.com>
2022-11-13 21:11:05 +0200

    Commit 3

--- hm_8/main.py
+++ hm_8/main.py
@@ -1,4 +1,6 @@
 mane_function():
     s = int(input("month: "))
-        if s == 12 or s == 1 or s == 2 :
+    if s == 12 or s == 1 or s == 2 :
         print("winter")
+    elif s >= 3 and s <= 5:
+        print("spring")  
