740385150c7c6801d24da8c257396947cad04115
Diana Volynets <d.volynets999@gmail.com>
2022-11-13 21:47:20 +0200

    Commit 1 PY_charm

--- hm_8/main.py
+++ hm_8/main.py
@@ -1,14 +1,17 @@
-mane_function():
+def mane_function():
     s = int(input("month: "))
-    if s == 12 or s == 1 or s == 2 :
-        print("winter")
-    elif s >= 3 and s <= 5:
-        print("spring")  
-    elif s >= 6 and s <= 8:
-        print("summer")
-test_function()
-    s = int(input("month: "))
-    if s == 12 or s == 1 or s == 2 :
+    if s == 12 or s == 1 or s == 2:
         print("winter")
     elif s >= 3 and s <= 5:
         print("spring")
+    elif s >= 6 and s <= 8:
+        print("summer")
+
+
+def test_function():
+    s = int(input("month: "))
+    if s == 12 or s == 1 or s == 2:
+        print("winter")
+    elif s >= 3 and s <= 5:
+        print("spring")
+
---
+++ hm_8/py_charm.py
@@ -0,0 +1,4 @@
+def season():
+    s = int(input("month: "))
+    if s == 12 or s == 1 or s == 2 :
+        print("winter")
