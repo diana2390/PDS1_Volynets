5507fe797ae0dc765e7c988e7eb59ed1dea2e55f
Diana Volynets <d.volynets999@gmail.com>
2022-11-13 21:18:44 +0200

    Test commit 1

--- hm_8/main.py
+++ hm_8/main.py
@@ -4,3 +4,7 @@ mane_function():
         print("winter")
     elif s >= 3 and s <= 5:
         print("spring")  
+test_function()
+    s = int(input("month: "))
+    if s == 12 or s == 1 or s == 2 :
+        print("winter")
