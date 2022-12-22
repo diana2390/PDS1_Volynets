6bc74b6c35b09ce96fbc96b614caddc6a5f2125f
Diana Volynets <d.volynets999@gmail.com>
2022-12-22 23:23:36 +0200

    hm_10

--- hm_11/hm11_3 server.py
+++ hm_11/hm11_3 server.py
@@ -1,14 +1,14 @@
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ser:
-ser.bind(('', 59000))
-ser.listen(10)
-print("Server is running")
-conn, addr = ser.accept()
-while True:
-    # print("connected:", addr)
-    data = conn.recv(1024).decode()
-    punc = ",.!?:; "
-    for i in punc:
-        data = data.replace(i, "")
-    data = str(len(data))
-    conn.send(data.encode())
+    ser.bind(('', 59000))
+    ser.listen(10)
+    print("Server is running")
+    conn, addr = ser.accept()
+    while True:
+        # print("connected:", addr)
+        data = conn.recv(1024).decode()
+        punc = ",.!?:; "
+        for i in punc:
+            data = data.replace(i, "")
+        data = str(len(data))
+        conn.send(data.encode())
