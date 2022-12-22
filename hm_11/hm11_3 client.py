e34278efdf378d03cc081b4858d07133c89219d9
Diana Volynets <d.volynets999@gmail.com>
2022-12-22 23:24:13 +0200

    hm_10

--- hm_11/hm11_3 client.py
+++ hm_11/hm11_3 client.py
@@ -1,6 +1,6 @@
 import socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as r:
-    r.connect(("localhost", 59000))
-    r.send("Hello, world".encode())
-    data = r.recv(1024).decode()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cl:
+    cl.connect(("localhost", 59000))
+    cl.send("Hello, world".encode())
+    data = cl.recv(1024).decode()
     print(data)
