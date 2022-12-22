74e090a698e051b114e649c7d22c0f2f9ca3aa43
Diana Volynets <d.volynets999@gmail.com>
2022-12-22 23:28:48 +0200

    hm_10

--- hm_11/hm11_1 client.py
+++ hm_11/hm11_1 client.py
@@ -1,8 +1,8 @@
 import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 57000))
-while True:
-    c = input("Client: ").encode()
-    client.send(c)
-    data = client.recv(1024)
-    print(f"Server`s message: {data.decode()}")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as k:
+    k.connect(("localhost", 57000))
+    while True:
+        c = input("Client: ").encode()
+        k.send(c)
+        data = k.recv(1024)
+        print(f"Server`s message: {data.decode()}")
--- hm_11/hm11_1 server.py
+++ hm_11/hm11_1 server.py
@@ -1,11 +1,11 @@
 import socket
-new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-new_socket.bind(('', 57000))
-new_socket.listen(10)
-print("Server is running")
-conn, addr = new_socket.accept()
-while True:
-    data = conn.recv(1024).decode()
-    print(f"Client`s message: {data}")
-    a = input("Server: ")
-    conn.send(a.encode())
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
+    f.bind(('', 57000))
+    f.listen(10)
+    print("Server is running")
+    conn, addr = f.accept()
+    while True:
+        data = conn.recv(1024).decode()
+        print(f"Client`s message: {data}")
+        a = input("Server: ")
+        conn.send(a.encode())
