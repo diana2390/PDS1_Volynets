aca164c11ffa73a032395a2c046d0e2fb4ff61b6
Diana Volynets <d.volynets999@gmail.com>
2022-12-22 23:27:02 +0200

    hm_10

--- hm_11/hm11_2 client.py
+++ hm_11/hm11_2 client.py
@@ -1,8 +1,8 @@
 import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 53000))
-while True:
-    c = input("Client: ").encode()
-    client.send(c)
-    data = client.recv(1024)
-    print(f"Server`s answer: {data.decode()}")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as y:
+    y.connect(("localhost", 53000))
+    while True:
+        c = input("Client: ").encode()
+        y.send(c)
+        data = y.recv(1024)
+        print(f"Server`s answer: {data.decode()}")
--- hm_11/hm11_2 server.py
+++ hm_11/hm11_2 server.py
@@ -1,17 +1,17 @@
 import socket
-new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-new_socket.bind(('', 53000))
-new_socket.listen(10)
-print("Server is running")
-conn, addr = new_socket.accept()
-while True:
-    data = conn.recv(1024).decode()
-    print(data)
-    if data == "I need help":
-        conn.send("ok".encode())
-    elif data == "I want cookies":
-        conn.send("10$".encode())
-    else:
-        print(f"Client`s message: {data}")
-        a = input("Server: ")
-        conn.send(a.encode())
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', 53000))
+    s.listen(10)
+    print("Server is running")
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024).decode()
+        print(data)
+        if data == "I need help":
+            conn.send("ok".encode())
+        elif data == "I want cookies":
+            conn.send("10$".encode())
+        else:
+            print(f"Client`s message: {data}")
+            a = input("Server: ")
+            conn.send(a.encode())
