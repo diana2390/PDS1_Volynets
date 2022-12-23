e18a8fae2f6d8a9542bc4b3ff7a5a75cb117a2c3
Diana Volynets <d.volynets999@gmail.com>
2022-12-23 23:14:03 +0200

    hm_12

--- hm_12/hm12_1.py
+++ hm_12/hm12_1.py
@@ -1,50 +1,36 @@
 import concurrent.futures
 import time
 
-# Calculate factorial function
+# ThreadPoolExecutor
 def factorial(number):
     if number == 1:
         return 1
     else:
         return number * factorial(number - 1)
 
-
-if __name__ == '__main__':
-    # ThreadPoolExecutor speed test
+if __name__=="__main__":
     start1 = time.perf_counter()
-
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        numbers = [_ for _ in range(1, 10)]
-        results = [executor.submit(factorial, num) for num in numbers]
-
-    for result in results:
-        print(result.result())
-
+        results = [executor.submit(factorial, 5) for _ in range(1,10)]
+    for i in results:
+        print(i.result())
     finish1 = time.perf_counter()
 
-    time_result1 = finish1 - start1
-    print(f"Time to execut: {time_result1} seconds\n")
+    time_res1 = finish1 - start1
+    print(f"ThreadPoolExecutor finished in {time_res1}second(s)")
 
-
-    # ProcessPoolExecutor speed test
+# ProcessPoolExecutor
     start2 = time.perf_counter()
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        numbers = [_ for _ in range(1, 10)]
-        results = [executor.submit(factorial, num) for num in numbers]
-
-    for result in results:
-        print(result.result())
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        results = [executor.submit(factorial, 5) for _ in range(1,10)]
+    for i in results:
+        print(i.result())
     finish2 = time.perf_counter()
 
-    time_result2 = finish2 - start2
-    print(f"Time to execut: {time_result2} seconds")
-
-
-    # Time speed comparison
-    if time_result1 < time_result2:
-        print(f'\nThe best method is ThreadPoolExecutor.\nIt tooks {time_result2 - time_result1} less seconds to execute!')
-
-    if time_result2 < time_result1:
-        print(f'\nThe best method is ProcessPoolExecutor.\nIt tooks {time_result1 - time_result2} less seconds to execute!')
+    time_res2 = finish2 - start2
+    print(f"ProcessPoolExecutor finished in {time_res2}second(s)")
+# Compare the time
+    if time_res1 < time_res2:
+        print(f"ThreadPoolExecutor is faster. Difference is {time_res2 - time_res1}")
+    if time_res2 < time_res1:
+        print(f"ProcessPoolExecutor is faster. Difference is {time_res1 - time_res2}" )
