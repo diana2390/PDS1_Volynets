7403765a05145da648002cec7e034a18c2ee9dd9
Diana Volynets <d.volynets999@gmail.com>
2022-12-25 00:26:35 +0200

    hm_12

--- hm_12/hm12_2 client.py
+++ hm_12/hm12_2 client.py
@@ -3,6 +3,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cl:
     cl.connect(("localhost", 51000))
     while True:
         data = input("Enter numbers: ")
-        c.send(data.encode())
+        cl.send(data.encode())
         data = cl.recv(1024)
         print(data.decode())
