c69f786a7811d314ec99004166bd6193d1cb41d4
Diana Volynets <d.volynets999@gmail.com>
2022-12-25 00:34:11 +0200

    hm_12

--- hm_12/hm12_2server.py
+++ hm_12/hm12_2server.py
@@ -17,18 +17,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ser:
             pl_num = nums[0] + nums[1]
             return pl_num
         async def minus(nums):
-            mn_num = nums[0] + nums[1]
+            mn_num = nums[0] - nums[1]
             return mn_num
         async def multi(nums):
-            mul_num = nums[0] + nums[1]
+            mul_num = nums[0] * nums[1]
             return mul_num
 
         loop = asyncio.get_event_loop()
         tasks = [loop.create_task(plus(num_list)),loop.create_task(minus(num_list)), loop.create_task(multi(num_list))]
         loop.run_until_complete(asyncio.wait(tasks))
 
-        results = f"Addition is: {tasks[0].result()};\n" \
-                  f'Subtraction values is {tasks[1].result()};\n' \
-                  f'Multiplication values is {tasks[2].result()}.'
+        results = f"Addition is: {tasks[0].result()}\n" \
+                  f'Subtraction values is {tasks[1].result()}\n' \
+                  f'Multiplication values is {tasks[2].result()}'
 
         conn.send(results.encode())
