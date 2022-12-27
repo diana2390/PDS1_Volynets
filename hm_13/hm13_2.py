933d6ef073456f6f7a940a2ccfcd194c0a987f00
Diana Volynets <d.volynets999@gmail.com>
2022-12-27 18:26:52 +0200

    hm_12

--- hm_13/hm13_2.py
+++ hm_13/hm13_2.py
@@ -1,10 +1,18 @@
 import random
+from random_words import RandomWords
 import time
 
 int_list = []
+float_list = []
+str_list = []
+
+w = RandomWords()
+
 for i in range(0, 5000):
     int_list.append(random.randint(1, 1000))
-# print(f"Sorting list:{int_list}")
+    float_list.append(random.uniform(0.1, 100))
+    str_list.append(w.random_word())
+
 # Bubble Gum
 def bubble_sort(data):
     length = len(data)
