63d6d59df4add8ab448a676d2326f291f16632a5
Diana Volynets <d.volynets999@gmail.com>
2022-12-27 22:34:23 +0200

    hm_12

--- hm_13/hm13_2.py
+++ hm_13/hm13_2.py
@@ -13,58 +13,61 @@ for i in range(0, 5000):
     float_list.append(random.uniform(0.1, 100))
     str_list.append(w.random_word())
 
-# Bubble Gum
-def bubble_sort(data):
-    length = len(data)
-    for i in range(length):
-        swapped = False
-        for i in range(0, length - i - 1):
-            if data[i] > data[i + 1]:
-                data[i], data[i + 1] = data[i + 1], data[i]
-                swapped = True
-        if not swapped:
-            break
-    print(f"Bubble Sort: {data}")
+# Bubble Sort:
+def bubble_sort(list, iter):
+    total_time = []
+    for i in range(iter):
+        start = time.perf_counter()
+        length = len(list)
+        for y in range(length):
+            for i in range(0, length - i - 1):
+                if list[i] > list[i + 1]:
+                    list[i], list[i + 1] = list[i + 1], list[i]
+    # print(f"Bubble Sort: {list}")
 
-cur_time = time.time()
-bubble_sort(int_list)
-print(f"Duration Bubble time: {time.time() - cur_time}")
+        finish = time.perf_counter()
+        cur_time = finish - start
+        total_time.append(cur_time)
+    print(f"Duration Bubble time: {sum(total_time) / len(total_time)} sec.")
+bubble_sort(int_list, 10)
+bubble_sort(float_list, 10)
+bubble_sort(str_list, 10)
 
 # Sorting
-def selection_sort(data):
-    for s in range (0, len(data)):
-        min = s
-        for fl in range (s +1,len(data)):
-            if data[fl] < data[min]:
-                min = fl
-            if min != s:
-                data[s], data[min] = data[min], data[s]
-    print(f"Selection Sort: {data}")
-
-cur_time = time.time()
-selection_sort(int_list)
-print(f"Duration Sorting time: {time.time() - cur_time}")
-
-# Qwuack
-def partition(nums, low, high):
-    p = nums[(low + high) // 2]
-    i - low - 1
-    j = high + 1
-    while True:
-        i += 1
-        while nums[i] < p:
-            i += 1
-        j -= 1
-        while nums[j] > p:
-            j -= 1
-    def _quick_sort(items, low, high):
-        if low < high:
-            spl = partition(items, low, high)
-            _quick_sort(items, low, spl)
-            _quick_sort(items, spl + 1, high)
-    _quick_sort(nums, 0, len(nums) - 1)
-
-cur_time = time.time()
-_quick_sort(int_list)
-print(f"Quick Sort: {int_list}")
-print(f"Durations time: {time.time() - cur_time}")
+# def selection_sort(data):
+#     for s in range (0, len(data)):
+#         min = s
+#         for fl in range (s +1,len(data)):
+#             if data[fl] < data[min]:
+#                 min = fl
+#             if min != s:
+#                 data[s], data[min] = data[min], data[s]
+#     print(f"Selection Sort: {data}")
+#
+# cur_time = time.time()
+# selection_sort(int_list)
+# print(f"Duration Sorting time: {time.time() - cur_time}")
+#
+# # Quick Sort
+# def partition(nums, low, high):
+#     p = nums[(low + high) // 2]
+#     i - low - 1
+#     j = high + 1
+#     while True:
+#         i += 1
+#         while nums[i] < p:
+#             i += 1
+#         j -= 1
+#         while nums[j] > p:
+#             j -= 1
+#     def _quick_sort(items, low, high):
+#         if low < high:
+#             spl = partition(items, low, high)
+#             _quick_sort(items, low, spl)
+#             _quick_sort(items, spl + 1, high)
+#     _quick_sort(nums, 0, len(nums) - 1)
+#
+# cur_time = time.time()
+# _quick_sort(int_list)
+# print(f"Quick Sort: {int_list}")
+# print(f"Durations time: {time.time() - cur_time}")
